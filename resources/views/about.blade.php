5a489fbaf20dd86b9934aac894e5fcda331270d5
LucaLF-dev <laficaraluca@gmail.com>
2023-12-07 18:03:02 +0100

    Add: text to about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,5 +7,25 @@
 </head>
 <body>
     <h1>ABOUT US</h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti aut veritatis rem amet delectus molestiae dolore quo libero aspernatur voluptas aliquam recusandae quos at reprehenderit, necessitatibus in eveniet quas minus.
+    Animi id iure distinctio velit? Eligendi ipsum illo numquam vel sunt corporis tempore maxime voluptates tempora accusamus. Nisi beatae laudantium reiciendis! Eaque nam eum ullam quod animi, error dignissimos recusandae.
+    Cumque eveniet adipisci, dolorem non error mollitia atque nam quaerat tempore et inventore dolorum accusantium quos corrupti molestias ipsum iste eaque? Optio, ipsa iste temporibus veniam numquam iusto maiores reprehenderit.
+    Facere animi natus error corrupti alias amet, dicta sed, necessitatibus perspiciatis dolores eaque quisquam minima omnis reiciendis nobis aspernatur odit laborum doloribus provident inventore. Veniam harum recusandae ex quidem corrupti!
+    Eaque nulla saepe velit quos incidunt? Et doloremque hic quod repellendus fugit tenetur vel excepturi aperiam at id corporis nostrum nihil vero, similique placeat, quisquam, exercitationem facere. Aliquid, est minima.
+    Sed repellat repellendus recusandae nobis eligendi voluptates rem ab nesciunt fuga sint itaque quae quis tempore ipsum ullam tempora, perspiciatis excepturi ipsa vel earum aliquid, dolores nihil. Non, pariatur ducimus.
+    Deserunt accusamus illo ab consequatur sit architecto odit possimus nihil commodi eos, necessitatibus maxime obcaecati dolor aspernatur incidunt rerum. Quaerat aspernatur facilis atque facere deleniti suscipit voluptates harum, vitae eum.
+    Illo facere temporibus ducimus alias distinctio amet, nulla delectus laudantium quasi a nobis cumque labore corporis ab eligendi minus. Nesciunt quis reiciendis quasi possimus rerum veniam placeat, qui ipsum similique!
+    Magni, consequuntur! Corporis magnam consequatur quaerat ratione est dicta eius veniam, sapiente, atque voluptatibus cum debitis eum ducimus repudiandae vero assumenda? Consequatur officiis impedit rem. Esse quia facere magnam iure.
+    A facere reprehenderit laboriosam modi quos culpa ipsam deserunt perspiciatis dolores voluptates, ducimus ut deleniti quisquam facilis magnam, totam consectetur cumque magni alias doloribus molestiae quo voluptas. Nam, molestiae rerum?
+    Amet consectetur nobis labore iste eveniet autem distinctio explicabo optio unde ipsa, recusandae a doloribus ratione mollitia quasi impedit quas commodi officiis tempore facere molestias cum. Tempore veniam dolore vel!
+    Officia, quae officiis, beatae eos sunt incidunt nihil atque saepe debitis in quasi animi nemo fugit nisi deserunt neque unde dolor! Explicabo rerum neque earum est ea quibusdam, id fugit.
+    Voluptates ut excepturi debitis porro autem, libero facilis tempora reiciendis eum quibusdam aliquid ullam! Adipisci, laudantium perspiciatis architecto error tempore, delectus optio et iusto sit perferendis eveniet voluptate libero quae.
+    Dolorem ad quod fugiat soluta iusto, aliquid iste architecto, laborum quaerat, maiores quibusdam. In veniam provident, possimus enim voluptatum expedita libero veritatis quos quia tempora corrupti vero placeat ab magni.
+    Quos natus alias a tenetur laudantium aut, veritatis sed possimus cumque ea accusantium. Fugiat rem voluptas praesentium fugit ullam optio. Ducimus amet magni repellendus commodi deserunt neque veritatis nemo nesciunt.
+    Impedit laboriosam velit quam atque. Voluptates tenetur, possimus libero expedita recusandae atque dolorem maxime consequatur tempora perferendis hic qui deleniti? Libero quam nihil omnis quaerat culpa provident recusandae. Provident, dolorem.
+    Illo a omnis earum nesciunt recusandae sapiente similique, eum voluptatem modi nobis fuga nemo impedit blanditiis quae fugit deserunt beatae quas ex architecto vitae praesentium aperiam reiciendis exercitationem aspernatur? Pariatur.
+    Molestiae nobis quam debitis repellendus non dolor provident corrupti vero dolorem perferendis nihil, laborum fugit nemo? Hic accusantium facilis corporis provident ratione reprehenderit earum maxime! Debitis voluptatem dolore pariatur nostrum.
+    Consequatur quidem eaque consequuntur ipsam adipisci officiis, hic inventore dolorem reiciendis quasi eveniet natus dolores, ratione minima eos aliquid quae. Inventore quas odit eos facilis ratione sapiente esse necessitatibus voluptas!
+    Repellendus dolor nobis a quos amet nemo iusto mollitia voluptates! Blanditiis, aut aliquid. Dolorum dignissimos laborum reprehenderit inventore? Consequatur est nobis sed unde architecto asperiores accusantium! Vel tenetur expedita modi!</p>
 </body>
 </html>
